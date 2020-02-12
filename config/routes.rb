a52a464b3342890b6f2813fc3f3458df5a079501
chimi555 <chinami.s515@gmail.com>
2020-02-12 15:02:20 +0900

    root-path変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
