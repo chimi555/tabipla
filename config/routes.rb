501087748ea57bb0ebf6d88a74407c5515a03840
chimi555 <chinami.s515@gmail.com>
2020-02-12 20:58:48 +0900

    sign_in,sign_out,sign_upのルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions',
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get '/signup', to: 'users/registrations#new'
+    get '/login', to: 'users/sessions#new'
+    post '/login', to: 'users/sessions#create'
+    delete '/logout', to: 'users/sessions#destroy'
+  end
 end
