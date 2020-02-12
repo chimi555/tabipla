940146abca6e3f6cf889c4cbef99c43bac7911a9
chimi555 <chinami.s515@gmail.com>
2020-02-12 20:16:27 +0900

    users controller,view追加、routes追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/about', to: 'static_pages#about' 
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
