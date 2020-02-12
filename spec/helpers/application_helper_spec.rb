91d38f5dc334630d0611a949c364cebd94e9eb6b
chimi555 <chinami.s515@gmail.com>
2020-02-12 16:08:48 +0900

    full_titleのテスト追加

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+RSpec.describe ApplicationHelper, type: :helper do
+  describe '#full_title' do
+    it "nilだった時、basetitleのみ返すこと" do
+      expect(full_title(nil)).to eq 'Trilog'
+    end
+
+    it "emptyだった時、basetitleのみ返すこと" do
+      expect(full_title('')).to eq 'Trilog'
+    end
+
+    it "正しいfulltitleを返すこと" do
+      expect(full_title('Test')).to eq 'Test - Trilog'
+    end
+  end
+end
