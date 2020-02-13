c70c403894edd634c3c6bf8a886d3f491fcb1e98
chimi555 <chinami.s515@gmail.com>
2020-02-13 14:08:17 +0900

    signup,login,edit後のリダイレクト先変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,4 +59,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  protected
+  
+  # アカウント登録後のリダイレクト先
+  def after_sign_up_path_for(resource)
+    user_path(resource.id)
+  end
+
+  # アカウント編集後のリダイレクト先
+  def after_update_path_for(resource)
+    user_path(resource.id)
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,10 +18,15 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource)
+    user_path(resource.id)
+  end
 end
