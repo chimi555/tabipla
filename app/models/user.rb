c5a9a07b4bdfae8a94f7153d2e2a97ab1add5172
chimi555 <chinami.s515@gmail.com>
2020-02-13 12:41:59 +0900

    ログインの認証条件をメールアドレスまたはユーザーネームに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :login
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
+
+  def login
+    @login || self.user_name || self.email
+  end
+
+  #ログイン認証の条件をオーバーライド
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(user_name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:user_name) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
