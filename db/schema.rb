f5056f5ba2e03dc3e0a9cf0edfc571f95d5f335a
chimi555 <chinami.s515@gmail.com>
2020-02-14 14:06:02 +0900

    facebooklogin設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_031422) do
+ActiveRecord::Schema.define(version: 2020_02_14_050119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2020_02_14_031422) do
     t.string "user_name"
     t.string "uid"
     t.string "provider"
-    t.string "name"
     t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
