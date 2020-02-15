b4019294f4bccb658dc54c7deb86a290370abacd
chimi555 <chinami.s515@gmail.com>
2020-02-15 12:36:38 +0900

    投稿ページ作成

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,21 @@
-# frozen_string_literal: true
-
 class TripsController < ApplicationController
+  def create
+    @trip = current_user.trips.build(trip_params)
+    if @trip.save
+      flash[:success] = '新しい旅行プランが登録されました'
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def new
+    @trip = current_user.trips.build if user_signed_in?
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,5 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:show]
+  resources :trips
 end
