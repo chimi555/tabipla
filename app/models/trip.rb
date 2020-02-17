53b126f0b883a48bf2bad3b7395e24aa257fe989
chimi555 <chinami.s515@gmail.com>
2020-02-17 19:45:03 +0900

    tripモデルにバリデーション追加

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,13 @@ class Trip < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :name, presence: true
+  validates :content, length: { maximum: 140 }
+  validate :picture_size
+
+  private
+
+  # アップロードされた画像のサイズをバリデーションする
+  def picture_size
+    errors.add(:picture, 'shoule be less than 5MB') if picture.size > 5.megabytes
+  end
 end
