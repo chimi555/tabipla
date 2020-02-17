5420f75c86e0f9d3ed08247c03550395d847d0b3
chimi555 <chinami.s515@gmail.com>
2020-02-17 14:05:43 +0900

    プロフィール画像のデフォルト画像を設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -18,13 +18,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     %w[jpg jpeg gif png]
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  # デフォルト画像
+  def default_url(*args)
+    [version_name, 'default.png'].compact.join('_')
+  end
 
   version :thumb do
     process resize_to_fill: [150, 150, 'Center']
