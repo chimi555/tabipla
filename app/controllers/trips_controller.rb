03ab7a0357041953156b558aca06cb38996ef65c
chimi555 <chinami.s515@gmail.com>
2020-02-18 12:14:49 +0900

    trip_showページ作成

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,13 @@
 class TripsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:update, :destroy]
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  def new
+    @trip = current_user.trips.build if user_signed_in?
+  end
+
   def create
     @trip = current_user.trips.build(trip_params)
     if @trip.save
@@ -8,10 +16,6 @@ class TripsController < ApplicationController
     else
       render 'static_pages/home'
     end
-  end
-
-  def new
-    @trip = current_user.trips.build if user_signed_in?
   end
 
   private
