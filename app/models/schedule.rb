4c97706f78538ec982e6e9205a9f8bde0a40af37
chimi555 <chinami.s515@gmail.com>
2020-02-19 18:31:21 +0900

    dateのバリデーション削除

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,10 +1,9 @@
 class Schedule < ApplicationRecord
-  #モデルの関連定義
+  # モデルの関連定義
   belongs_to :trip
 
-  #バリデーション
+  # バリデーション
   validates :trip_id, presence: true
-  validates :date, presence:true
-  validates :place, presence:true
+  validates :place, presence: true, length: { maximum: 30 }
   validates :memo, length: { maximum: 140 }
 end
