f3b0e08d47098c095c635693175c3c9d6ef1ce46
chimi555 <chinami.s515@gmail.com>
2020-02-21 11:46:05 +0900

    likesコントローラースペック追加

---
+++ spec/controllers/likes_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe LikesController, type: :controller do
+  let!(:user) { create(:user) }
+  let!(:trip) { create(:trip) }
+  
+
+  describe '#create' do
+    context 'ログイン済ユーザー' do
+      before do
+        sign_in user
+      end
+
+     # it 'お気に入り登録できる' do
+     #  like_params = attributes_for(:like, {
+     #    trip_id: trip.id,
+     #  })
+     #  expect do
+     #    post :create, params: { like: like_params }
+     #  end.to change(user.likes, :count).by(1)
+     # end
+    end
+
+    context 'ログインしていないユーザー' do
+      it 'お気に入り登録できない' do
+        like_params = attributes_for(:like, {
+          trip_id: trip.id,
+        })
+        expect do
+          post :create, params: { like: like_params }
+        end.not_to change(user.likes, :count)
+      end
+    end
+  end
+
+end
