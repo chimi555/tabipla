3b33c699606b39231c426ff2fbb68964d002b6fc
chimi555 <chinami.s515@gmail.com>
2020-02-24 15:08:38 +0900

    dayモデルからhas_many notes削除

--- app/models/day.rb
+++ app/models/day.rb
@@ -3,8 +3,6 @@ class Day < ApplicationRecord
   belongs_to :trip
   has_many :schedules, dependent: :destroy
   accepts_nested_attributes_for :schedules, allow_destroy: true
-  has_many :notes, dependent: :destroy
-  accepts_nested_attributes_for :notes, allow_destroy: true
   # scope
   default_scope -> { order(id: :asc) }
 end
