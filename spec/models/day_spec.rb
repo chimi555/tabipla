6119f7fb099cd90cef2170d8887f69df458169b4
chimi555 <chinami.s515@gmail.com>
2020-02-24 22:03:04 +0900

    アソシエーションのテスト追加

--- spec/models/day_spec.rb
+++ spec/models/day_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe Day, type: :model do
       expect(day).to be_valid
     end
   end
+
+  context 'アソシエーションのテスト' do
+    it 'tripが削除されると、dayも削除される' do
+      expect {day}.to change {Day.count}.by(+1)
+      day.trip.destroy
+      expect(Trip.count).to eq 0
+      expect(Day.count).to eq 0
+    end
+  end
 end
