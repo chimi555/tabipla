0524d3159c7eae9bb5467fcde6bc4b507d7e1412
chimi555 <chinami.s515@gmail.com>
2020-02-28 18:18:17 +0900

    bullet設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,15 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
+
+  # bullet設定
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer   = true
+    Bullet.add_whitelist type: :unused_eager_loading, class_name: "Spree::Variant", association: :images
+  end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -45,4 +45,12 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # bullet設定
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.raise = true # raise an error if n+1 query occurs
+    Bullet.add_whitelist type: :unused_eager_loading, class_name: "Spree::Variant", association: :images
+  end 
 end
