3a6dd36d52340478b60571018691d57175e707a5
chimi555 <chinami.s515@gmail.com>
2020-03-01 15:08:43 +0900

    RelationshipモデルとUserモデルの関連付け

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,9 @@
 class Relationship < ApplicationRecord
+  # モデルの関連定義
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+  # バリデーション
+  validates :trip_id, presence: true
+  validates :tag_id, presence: true
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
   has_many :trips, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_trips, through: :likes, source: :trip
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   # devise
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable,
