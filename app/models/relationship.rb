d9ff26d144a35fedc718caf4ede60a5e5d23a0d9
chimi555 <chinami.s515@gmail.com>
2020-03-01 21:09:20 +0900

    バリデーション修正

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,6 +3,6 @@ class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
   # バリデーション
-  validates :trip_id, presence: true
-  validates :tag_id, presence: true
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
