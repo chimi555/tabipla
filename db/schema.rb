d40ec05b3d0d1fa76ded13f895159b2ebedd4553
chimi555 <chinami.s515@gmail.com>
2020-03-01 14:54:17 +0900

    relationshipモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_104520) do
+ActiveRecord::Schema.define(version: 2020_03_01_055103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(version: 2020_02_28_104520) do
     t.datetime "updated_at", null: false
     t.index ["trip_id", "created_at"], name: "index_notes_on_trip_id_and_created_at"
     t.index ["trip_id"], name: "index_notes_on_trip_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "schedules", force: :cascade do |t|
