44a2b1afd82983c4da67e63ea38776e5bf95bc96
chimi555 <chinami.s515@gmail.com>
2020-03-02 16:33:15 +0900

    お気に入り機能のコントローラースペック追加

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -1,32 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe LikesController, type: :controller do
-  let!(:user) { create(:user) }
-  let!(:trip) { create(:trip) }
+  let(:user) { create(:user) }
+  let(:trip) { create(:trip) }
+  let(:like) { create(:like, user: user, trip: trip) }
 
   describe '#create' do
-    # context 'ログイン済ユーザー' do
-    #  before do
-    #    sign_in user
-    #  end
+    context 'ログイン済ユーザー' do
+      before do
+        sign_in user
+      end
 
-    # it 'お気に入り登録できる' do
-    #  like_params = attributes_for(:like, {
-    #    trip_id: trip.id,
-    #  })
-    #  expect do
-    #    post :create, params: { like: like_params }
-    #  end.to change(user.likes, :count).by(1)
-    # end
-    # end
+      it 'お気に入り登録できること' do
+        expect do
+          post :create, params: { trip_id: trip.id }
+        end.to change(user.likes, :count).by(1)
+      end
+
+      it 'Ajaxによるお気に入り登録ができること' do
+        expect do
+          post :create, params: { trip_id: trip.id }, xhr: true
+        end.to change(user.likes, :count).by(1)
+      end
+    end
 
     context 'ログインしていないユーザー' do
-      it 'お気に入り登録できない' do
-        like_params = attributes_for(:like, {
-          trip_id: trip.id,
-        })
+      it 'お気に入り登録できないこと' do
         expect do
-          post :create, params: { like: like_params }
+          post :create, params: { trip_id: trip.id }
+        end.not_to change(user.likes, :count)
+      end
+    end
+  end
+
+  describe '#destroy' do
+    context 'ログイン済ユーザー' do
+      before do
+        sign_in user
+      end
+
+      it 'お気に入り解除できること' do
+        like
+        expect do
+          delete :destroy, params: { id: like.id }
+        end.to change(user.likes, :count).by(-1)
+      end
+
+      it 'Ajaxによるお気に入り解除ができること' do
+        like
+        expect do
+          delete :destroy, params: { id: like.id }, xhr: true
+        end.to change(user.likes, :count).by(-1)
+      end
+    end
+
+    context 'ログインしていないユーザー' do
+      it 'お気に入り解除できないこと' do
+        like
+        expect do
+          delete :destroy, params: { id: like.id }
         end.not_to change(user.likes, :count)
       end
     end
