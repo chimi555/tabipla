27bebe7a27096ab921906a599d887f48a1a5c0e7
chimi555 <chinami.s515@gmail.com>
2020-03-02 19:24:12 +0900

    フォロー機能コントローラースペック追加

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -15,6 +15,12 @@ RSpec.describe RelationshipsController, type: :controller do
           post :create, params: { followed_id: other_user.id }
         end.to change(user.following, :count).by(1)
       end
+
+      it 'Ajaxによるフォローができること' do
+        expect do
+          post :create, params: { followed_id: other_user.id }, xhr: true
+        end.to change(user.following, :count).by(1)
+      end
     end
 
     context 'ログインしていないユーザー' do
@@ -38,6 +44,14 @@ RSpec.describe RelationshipsController, type: :controller do
         relationship = user.active_relationships.find_by(followed_id: other_user.id)
         expect do
           delete :destroy, params: { id: relationship.id }
+        end.to change(user.following, :count).by(-1)
+      end
+
+      it 'Ajaxによるアンフォローができること' do
+        user.follow(other_user)
+        relationship = user.active_relationships.find_by(followed_id: other_user.id)
+        expect do
+          delete :destroy, params: { id: relationship.id }, xhr: true
         end.to change(user.following, :count).by(-1)
       end
     end
