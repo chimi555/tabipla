84f75c9fe36db1452a38bd03906456f3aad15a31
chimi555 <chinami.s515@gmail.com>
2020-03-03 11:45:58 +0900

    text_url_to_linkのhelperスペック追加

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,7 +1,29 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
+
+  describe '#text_url_to_link' do
+    it 'httpがついていればハイパーリンクで返すこと' do
+      text = "http://example.com"
+      expect(text_url_to_link(text)).to have_link
+    end
+
+    it 'httpsがついていればハイパーリンクで返すこと' do
+      text = "https://example.com"
+      expect(text_url_to_link(text)).to have_link
+    end
+
+    it 'httpがついていなければリンクにならないこと' do
+      text = "リンクなしテキスト"
+      expect(text_url_to_link(text)).not_to have_link
+    end
+
+    it 'httpのついたURLの後にテキストがあるときURLのみハイパーリンクで返すこと' do
+      text = "http://example.com テキスト"
+      expect(text_url_to_link(text)).to have_link 'http://example.com', href: 'http://example.com'
+      expect(text_url_to_link(text)).not_to have_link 'テキスト'
+    end
+  end
+
   describe '#full_title' do
     it 'nilだった時、basetitleのみ返すこと' do
       expect(full_title(nil)).to eq 'Trilog'
