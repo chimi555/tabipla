c63de62f2caf121cc7b1594fc6c48c8111732bac
chimi555 <chinami.s515@gmail.com>
2020-03-08 21:24:52 +0900

    タブ表示に変更したことによりいらなくなったページ、アクション、ルーティングを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,24 +25,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def like
-    @user_likes = current_user.liked_trips_list.page(params[:page]).per(10)
-  end
-
-  def following
-    @title = "フォロー中"
-    @user = User.find(params[:id])
-    @users = @user.following.page(params[:page])
-    render 'show_follow'
-  end
-
-  def followers
-    @title = "フォロワー"
-    @user = User.find(params[:id])
-    @users = @user.followers.page(params[:page])
-    render 'show_follow'
-  end
-
   private
 
   def password_params
--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,6 @@ Rails.application.routes.draw do
     member do
       get 'password_edit', to: 'users#password_edit'
       patch 'password_update', to: 'users#password_update'
-      get 'like', to: 'users#like'
-      get :following, :followers
     end
   end
   resources :trips
