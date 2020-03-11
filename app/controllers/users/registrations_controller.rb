9b01b7d1c9438f6b41f133a804d8a148c5cba944
chimi555 <chinami.s515@gmail.com>
2020-03-11 11:50:33 +0900

    テストユーザーはdestroyアクションの権限も与えない

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,8 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :guest_judge, only: [:edit, :update]
-  before_action :admin_judge, only: [:destroy]
+  before_action :guest_judge, only: [:edit, :update, :destroy]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
